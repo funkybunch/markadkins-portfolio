a8924ae284961c41ddbd7d3581c0b09a5c8f2461
Mark Adkins <mark@bymark.co>
2019-12-02 20:35:26 -0500

    Data Source Update

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const cron = require('node-cron');
-const parse = require('node-html-parser');
+const parseHTML = require('node-html-parser');
+const ParseXML = require('rss-parser');
 const getJSON = require('get-json');
+const wiki = require('wikijs').default;
 const writeJSON = require('write-json-file');
+const Twitter = require('twitter');
+
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -26,9 +30,77 @@ function getWeather() {
   });
 }
 
+function getSupermoon() {
+  getJSON('https://isitasupermoon.com/api/v1.0/', function(error, response){
+    writeJSON('./data/supermoon.json', response);
+  });
+}
+
+function getTweets() {
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  client.get('statuses/user_timeline', {screen_name: "markadkn", count: 10}, function(error, tweets, response) {
+    if(!error) {
+      writeJSON('./data/tweets.json', tweets);
+    }
+  });
+}
+
+function getWikis() {
+  let wikiData = {};
+
+  // Articles we're searching
+  wikiData.TLE = {};
+  wikiData.CAPCOM = {};
+
+  wiki()
+      .page('Two-line element set')
+      .then(page => {
+        wikiData.TLE.title = page.raw.title;
+        wikiData.TLE.url = page.raw.fullurl;
+        wikiData.TLE.updated = page.raw.touched;
+        return page;
+      })
+      .then(page => page.summary())
+      .then(summary => {
+        wikiData.TLE.summary = summary;
+        writeJSON('./data/wiki-tle.json', wikiData.TLE);
+      });
+
+  wiki()
+      .page('Flight_controller')
+      .then(page => {
+        wikiData.CAPCOM.url = page.raw.fullurl;
+        wikiData.CAPCOM.updated = page.raw.touched;
+        return page;
+      })
+      .then(page => page.sections())
+      .then(sections => {
+        wikiData.CAPCOM.title = sections[1].items[2].title;
+        wikiData.CAPCOM.summary = sections[1].items[2].content;
+        writeJSON('./data/wiki-capcom.json', wikiData.CAPCOM);
+      });
+}
+
+function getMediumFeed() {
+  let parser = new ParseXML();
+  parser.parseURL('https://medium.com/feed/@markadkins/', function(err, feed) {
+    writeJSON('./data/blogFeed.json', feed);
+  })
+}
+
 function getAllData() {
   console.log("Data fetched");
   getWeather();
+  getSupermoon();
+  getTweets();
+  getWikis();
+  getMediumFeed();
 }
 
 // Initial Data Setup
