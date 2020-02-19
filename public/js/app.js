7883a61502d9565685d1ab56ac84b82f619f4f18
Mark Adkins <mark@bymark.co>
2020-02-18 21:45:15 -0500

    Dynamic Component POC
    Getting JSON from URL to populate & generate components - proof of concept.

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import cardContentJSON from './data.json'
 require("./analytics");
 
 new Vue({
     el: '#app',
     data: {
         cardContent: {
-            title: "Card Title"
+            title: "Card Title",
+            items: [
+                {
+                    icon: "design",
+                    color: "#5d1abb",
+                    title: cardContentJSON.jobs[0].position,
+                    action: "View Description",
+                    category: "Design",
+                    modal: {
+
+                    }
+                }
+            ]
         }
     },
     methods: {},
