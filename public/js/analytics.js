e5efd7594e5768c60bfc68b33143e07f11f0c541
Mark Adkins <mark@bymark.co>
2020-02-28 22:08:15 -0500

    Enable Google Analytics
    Enables Google Analytics but respects the Do Not Track setting of browsers.

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,4 +1,14 @@
 // Respect Browser's Do Not Track Preference
-if(navigator.doNotTrack !== 1) {
+if(navigator.doNotTrack !== "1") {
+    let recaptchaScript = document.createElement('script');
+    recaptchaScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-43059653-8');
+    document.head.appendChild(recaptchaScript);
 
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-43059653-8');
+} else {
+    console.log("Did not load analytics per request of the browser's Do Not Track setting.");
 }
