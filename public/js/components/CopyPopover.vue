1d37304b19574157c62d31bf9d5509587c18b3c0
Mark Adkins <mark@bymark.co>
2020-07-31 17:26:08 -0400

    Voice Over Alerts

--- public/js/components/CopyPopover.vue
+++ public/js/components/CopyPopover.vue
@@ -5,9 +5,9 @@
             v-clipboard:success="clipboardSuccessHandler"
             v-clipboard:error="clipboardErrorHandler"
             v-on:keyup.enter.prevent="showPopover"
-            v-on:keyup.space.prevent="showPopover"
+            v-on:keyup.space="showPopover"
             role="button"
-            :aria-label="'Copy ' + copyValue + ' to clipboard.'"
+            :aria-label="'Press the space or enter key to copy ' + copyValue + ' to clipboard.'"
             tabindex="0"
             aria-pressed="false" aria-describedby="footer-copy-status"></button>
         <span v-html="hoverLabel" id="footer-copy-status"></span>
@@ -26,26 +26,27 @@
         },
         methods: {
             clipboardSuccessHandler ({ value, event }) {
-                this.$data.hoverLabel = "<i class='fal fa-check-circle success' role='alert'></i>&nbsp;Copied"
+                this.$data.hoverLabel = "</span><i class='fal fa-check-circle success'></i>&nbsp;Copied";
+                this.$announcer.polite('Copied to clipboard');
                 this.showPopover();
                 setTimeout(this.resetCopyLabel, 7000);
             },
             clipboardErrorHandler ({ value, event }) {
-                this.$data.hoverLabel = "<i class='fal fa-times-circle error' role='alert'></i>&nbsp;Failed to copy"
+                this.$data.hoverLabel = "<i class='fal fa-times-circle error'></i>&nbsp;Failed to copy";
+                this.$announcer.polite('Failed to copy to clipboard');
                 this.showPopover();
                 setTimeout(this.resetCopyLabel, 7000);
             },
             showPopover() {
                 let self = this;
                 self.$data.hoverOverride = true;
-                console.log("should show popover");
                 setTimeout(function() {
                   self.$data.hoverOverride = false;
-                  console.log("should hide popover");
                 }, 5000);
             },
             resetCopyLabel() {
-              this.$data.hoverLabel = "Click to copy to clipboard"
+              this.$data.hoverLabel = "Click to copy to clipboard";
+              this.$data.roleType = "";
             }
         }
     }
