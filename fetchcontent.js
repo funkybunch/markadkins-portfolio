0b12ece271f13acc13f97aa829b2c662e7d2f3d8
Mark Adkins <git.in.touch@adkins.coffee>
2024-06-09 22:54:55 -0400

    Update fetchcontent.js

--- fetchcontent.js
+++ fetchcontent.js
@@ -5,6 +5,20 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
+let routes = ['/'];
+
+function urlify(text) {
+    return text.toLowerCase().replaceAll(' ', '-');
+}
+
 axios.get(process.env.VITE_CMS).then(function(data) {
-    fs.writeFileSync(path.join(__dirname, 'src/js/content.json'), JSON.stringify(data.data));
+    const content = data.data;
+    fs.writeFileSync(path.join(__dirname, 'src/js/content.json'), JSON.stringify(content));
+    for(let company in content.experience) {
+        routes.push('/experience/' + urlify(content.experience[company].attributes.company));
+    }
+    for(let institution in content.education) {
+        routes.push('/education/' + urlify(content.education[institution].attributes.institution));
+    }
+    console.log(routes)
 });
